a13af3194cf75a676ed60549bb3e4ec0320ab77e
kerim1 <kerim.meijer@di.huc.knaw.nl>
2022-09-13 09:10:05 +0200

    New version with updated dependencies

--- src/main/java/nl/knaw/huygens/timbuctoo/remote/rs/download/RemoteFile.java
+++ src/main/java/nl/knaw/huygens/timbuctoo/remote/rs/download/RemoteFile.java
@@ -1,32 +1,67 @@
 package nl.knaw.huygens.timbuctoo.remote.rs.download;
 
 import nl.knaw.huygens.timbuctoo.remote.rs.download.exceptions.CantRetrieveFileException;
-import org.immutables.value.Value;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
-@Value.Immutable
-public interface RemoteFile {
-  String getUrl();
+public class RemoteFile {
+  private final String url;
+  private final RemoteData data;
+  private final String mimeType;
+  private final Metadata metadata;
 
-  RemoteData getData();
+  public RemoteFile(String url, RemoteData data, String mimeType, Metadata metadata) {
+    this.url = url;
+    this.data = data;
+    this.mimeType = mimeType;
+    this.metadata = metadata;
+  }
 
-  String getMimeType();
+  public String getUrl() {
+    return url;
+  }
 
-  Metadata getMetadata();
+  public RemoteData getData() {
+    return data;
+  }
+
+  public String getMimeType() {
+    return mimeType;
+  }
+
+  public Metadata getMetadata() {
+    return metadata;
+  }
 
   static RemoteFile create(String url, RemoteData data, String mimeType, Metadata metadata) {
-    return ImmutableRemoteFile.builder()
-      .data(data)
-      .url(url)
-      .mimeType(mimeType)
-      .metadata(metadata)
-      .build();
+    return new RemoteFile(url, data, mimeType, metadata);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    RemoteFile that = (RemoteFile) o;
+    return Objects.equals(url, that.url) &&
+            Objects.equals(data, that.data) &&
+            Objects.equals(mimeType, that.mimeType) &&
+            Objects.equals(metadata, that.metadata);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(url, data, mimeType, metadata);
   }
 
   interface RemoteData {
     InputStream get() throws IOException, CantRetrieveFileException;
   }
 }
-
